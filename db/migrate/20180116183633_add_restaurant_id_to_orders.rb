387cc191c15f8b20bec7181b4da94b1dd7517718
christykusuma <christykusuma96@gmail.com>
2018-01-16 13:37:44 -0500

    add migration restaurants to orders table

---
+++ db/migrate/20180116183633_add_restaurant_id_to_orders.rb
@@ -0,0 +1,4 @@
+class AddRestaurantIdToOrders < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107204259) do
+ActiveRecord::Schema.define(version: 20180116183633) do
 
   create_table "menu_item_orders", force: :cascade do |t|
     t.integer "order_id"
